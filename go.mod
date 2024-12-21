d615195c72d4e1549d5ab9469d5c97e5acdcc1ab
felipeversiane <felipeversiane09@gmail.com>
2024-12-20 22:53:45 -0300

    feat: add user entity

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/felipeversiane/go-boiterplate
 
 go 1.22.0
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+)
---
+++ internal/user.go
@@ -0,0 +1,41 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        string 
+	Email     string
+	FirstName string
+	LastName  string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Deleted   bool
+}
+
+func NewUser(email, firstName, lastName, password string) User {
+	return User{
+		ID:        uuid.NewString(),
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Password:  password,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Deleted:   false,
+	}
+}
+
+func (user *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	return nil
+}
